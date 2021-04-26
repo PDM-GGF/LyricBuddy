4910cc3e64bf31e77eccb671efc4a115812d026c
Riccardo Galbusera <ricc98@gmail.com>
2021-04-26 18:01:24 +0200

    fixed navbar

--- app/src/main/java/com/progettopdm/lyricbuddy/ui/search/SearchFragment.java
+++ app/src/main/java/com/progettopdm/lyricbuddy/ui/search/SearchFragment.java
@@ -1,5 +1,6 @@
 package com.progettopdm.lyricbuddy.ui.search;
 
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -11,28 +12,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.progettopdm.lyricbuddy.R;
+import com.progettopdm.lyricbuddy.ui.search.SearchViewModel;
 
 public class SearchFragment extends Fragment {
 
-    private SearchViewModel mViewModel;
+    private SearchViewModel searchViewModel;
 
-    public static SearchFragment newInstance() {
-        return new SearchFragment();
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        searchViewModel =
+                new ViewModelProvider(this).get(SearchViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_search, container, false);
+        final TextView textView = root.findViewById(R.id.text_search);
+        searchViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
     }
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.search_fragment, container, false);
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(SearchViewModel.class);
-        // TODO: Use the ViewModel
-    }
-
 }
