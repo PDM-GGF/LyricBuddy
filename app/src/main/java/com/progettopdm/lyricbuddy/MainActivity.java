d502571f8118e5197e642e2f687c02f6feb5569f
gigal <gigaluca99@gmail.com>
2021-04-28 11:44:10 +0200

    creazione userprofile (not working)

--- app/src/main/java/com/progettopdm/lyricbuddy/MainActivity.java
+++ app/src/main/java/com/progettopdm/lyricbuddy/MainActivity.java
@@ -27,18 +27,18 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_search, R.id.navigation_favorites)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-       /* NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);*/
+        /*NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);*/
         NavigationUI.setupWithNavController(navView, navController);
 
         ImageView rightIcon = findViewById(R.id.right_icon);
         TextView title = findViewById(R.id.toolbar_title);
 
-        rightIcon.setOnClickListener(new View.OnClickListener() {
+        /*rightIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "you clicked in profile", Toast.LENGTH_SHORT).show();
             }
-        });
+        });*/
 
         title.setText("LyricBuddy");
     }
---
+++ app/src/main/java/com/progettopdm/lyricbuddy/ui/userprofile/UserProfileViewModel.java
@@ -0,0 +1,18 @@
+package com.progettopdm.lyricbuddy.ui.userprofile;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class UserProfileViewModel extends ViewModel {
+    private MutableLiveData<String> mText;
+
+    public UserProfileViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is profile user fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
