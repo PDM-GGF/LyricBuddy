fa3e5d55aad4ca853cc93c018ad935b6030f4920
gigal <gigaluca99@gmail.com>
2021-05-22 16:54:52 +0200

    dipendenze retrofit, classe costanti, interfaccia per API

---
+++ app/src/main/java/com/progettopdm/lyricbuddy/model/Response.java
@@ -0,0 +1,31 @@
+package com.progettopdm.lyricbuddy.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Response implements Parcelable {
+
+    protected Response(Parcel in) {
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Response> CREATOR = new Creator<Response>() {
+        @Override
+        public Response createFromParcel(Parcel in) {
+            return new Response(in);
+        }
+
+        @Override
+        public Response[] newArray(int size) {
+            return new Response[size];
+        }
+    };
+}
---
+++ app/src/main/java/com/progettopdm/lyricbuddy/services/LyricBuddyService.java
@@ -0,0 +1,14 @@
+package com.progettopdm.lyricbuddy.services;
+
+import com.progettopdm.lyricbuddy.model.Response;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Query;
+
+public interface LyricBuddyService {
+
+
+
+
+}
