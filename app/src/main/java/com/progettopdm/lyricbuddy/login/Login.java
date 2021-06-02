5f27c908f89efb6b14ffbecf8b06582f796423af
gigal <gigaluca99@gmail.com>
2021-06-02 19:32:15 +0200

    splash screen, connection firebase, activity login e register

---
+++ app/src/main/java/com/progettopdm/lyricbuddy/login/Login.java
@@ -0,0 +1,27 @@
+package com.progettopdm.lyricbuddy.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.progettopdm.lyricbuddy.R;
+
+public class Login extends AppCompatActivity {
+    Button signUpButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        signUpButton = findViewById(R.id.button_signup);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Register.class));
+            }
+        });
+    }
+}
